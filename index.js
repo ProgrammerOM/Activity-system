f8da573a30a0454c05aeb5b9f443365d175935fa
Zayden4159 <ddionn7@gmail.com>
2024-02-13 14:26:47 +0700

    New

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 
 const port = 5500;
 
+app.use(express.static('./'));
+
 app.get("/", (req, res) => {
-    render('index.html')
+  res.render("index.html");
 });
 
 app.listen(port, () => {
