710a680029e7aac16cf3290fb0e891bcf1b495c7
ProgrammerOM <spd9901021995@gmail.com>
2024-06-17 14:56:30 +0700

    Update

--- routers/routers.js
+++ routers/routers.js
@@ -1,25 +1,10 @@
 const express = require("express");
-const network = require("network");
 const router = express.Router();
 
-const Engines = require("../controllers/SearchEngine");
 const CheckUser = require('../controllers/CheckUser')
 
-router.post("/log-referrer", Engines.CheckEngines);
 
 router.post('/redeem',CheckUser.CheckUser)
-router.get("/get-mac", (req, res) => {
-  network.get_active_interface((err, obj) => {
-    if (err) {
-      console.log(err);
-      res.status(500).json(err);
-    } else {
-      res.json({ mac_address: obj.mac_address });
-    }
-  });
-});
 
-// router.post("/user", CheckUser);
-// router.post("/codes", RandomCode);
 
 module.exports = router;
