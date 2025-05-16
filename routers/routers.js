9fcc4cc38686caa203e0142e83c74c5f7e2f055a
Zayden4159 <ddionn7@gmail.com>
2025-05-16 16:16:23 +0700

    Add jackpot route to handle jackpot reviews in the router

--- routers/routers.js
+++ routers/routers.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 // const CheckUser = require("../controllers/CheckUser");
 const Free = require("../controllers/FreeCreditCtrl");
+const jack = require('../controllers/jackpotController')
 
 // router.post("/redeem", CheckUser.CheckUser);
 router.post("/free-credit", Free.FreeCredit);
 router.post("/update-status", Free.UpdateStatus);
 router.get("/codes", Free.SendClient);
+router.post("/api/reviews/jackpot", jack.jackpot);
 
 module.exports = router;
